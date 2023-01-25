22cd1aa80e0b928a9845aa93538aa9fab8546d5c
Omie Sawie <omkarsawant20HG@gmail.com>
2023-01-25 23:00:57 +0530

    changes to the findDetection.py file

--- contourDetection/contourDetection.py
+++ contourDetection/contourDetection.py
@@ -1,66 +1,62 @@
+#!/usr/bin/env python
+# coding: utf-8
+
 import cv2
 import numpy as np
-import math 
+import math
 
 
-def findContours(mask,blurred_frame):
+def findContours(mask, blurred_frame):
 
-    contours,_ = cv2.findContours(mask,cv2.RETR_CCOMP, cv2.CHAIN_APPROX_SIMPLE)
-     
+    contours, _ = cv2.findContours(mask, cv2.RETR_CCOMP,
+                                   cv2.CHAIN_APPROX_SIMPLE)
 
-    for contour in contours:
-        # C = max(contours,key=cv2.contourArea)
+    if len(contours) > 0:
+        C = max(contours, key=cv2.contourArea)
+        M = cv2.moments(C)
 
-        M = cv2.moments(contour)
-
-        if M["m00"]!=0 and cv2.contourArea(contour)>6000:
+        if M["m00"] != 0:
             Gx = int(M["m10"] / M["m00"])
             Gy = int(M["m01"] / M["m00"])
-            
-            cv2.drawContours(blurred_frame,contour,-1,(0,0,100),2)
-            
-            cv2.circle(blurred_frame, (Gx, Gy), 7, (255, 0, 0), -1)
- 
-            cv2.putText(blurred_frame, "Centroid", (Gx - 25, Gy - 25),  cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255, 00, 100), 2)
-
-
         else:
-            Gx=None
-            Gy=None
-            
-        # if Gx >= 350:
-            # print("right")
-# 
-        # if Gx < 350 and Gx > 290:
-            # print("straight")
-                # 
-        # if Gx <= 290:
-            # print("left")
+            Gx = None
+            Gy = None
 
-    cv2.imshow('webcam feed' , blurred_frame)
-    cv2.imshow('mask',mask)
-    cv2.imwrite("outpput_by_contour.png",blurred_frame)
+        if Gx >= 350:
+            print("right")
+
+        if Gx < 350 and Gx > 290:
+            print("straight")
+
+        if Gx <= 290:
+            print("left")
+
+        cv2.circle(blurred_frame, (Gx, Gy), 5, (255, 255, 255), -1)
+
+        cv2.putText(blurred_frame, "Centroid", (Gx - 25, Gy - 25),
+                    cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255, 255, 255), 2)
+
+        cv2.drawContours(blurred_frame, C, -1, (0, 0, 200), 3)
+    cv2.imshow('webcam feed', blurred_frame)
+    cv2.imshow('mask', mask)
 
 
-# cap = cv2.VideoCapture(0)
-while True: 
-    # ret, frame = cap.read()
-    
-    frame = cv2.imread("theme_arena_initial.png")
-    # frame = cv2.resize(frame,(640,480))
+cap = cv2.VideoCapture(0)
+while True:
+    ret, frame = cap.read()
 
-    blurred_frame = cv2.GaussianBlur(frame,(5,5),0)
-    
-    lower_black = np.array([0,100,0])
-    upper_black = np.array([0,255,255])
+    frame = cv2.resize(frame, (640, 480))
 
-    mask = cv2.inRange(frame,lower_black,upper_black)
-    findContours(mask,blurred_frame)
- 
+    blurred_frame = cv2.GaussianBlur(frame, (5, 5), 0)
+
+    lower_black = np.array([0, 0, 0])
+    upper_black = np.array([50, 50, 50])
+
+    mask = cv2.inRange(frame, lower_black, upper_black)
+    findContours(mask, blurred_frame)
 
     if cv2.waitKey(10) & 0xFF == ord(' '):
         break
-    
-# cap.release()
-cv2.destroyAllWindows()
 
+cap.release()
+cv2.destroyAllWindows()
---
+++ contourDetection/readme.md
@@ -0,0 +1,9 @@
+Install dependencies: <br>
+
+pip install opencv-python
+<br><br>
+then run:<br>
+python masker.py #to bring up the calibration menu
+
+else run:<br>
+python contourDetection.py #To find contours
