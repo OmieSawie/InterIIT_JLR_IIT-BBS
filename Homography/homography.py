6c9e9c406c9f757d8cdeaed43b5a8d0cd5f0f2ed
Omie Sawie <omkarsawant20HG@gmail.com>
2023-02-03 01:33:52 +0530

    Some More optimization before sleeping

--- Homography/homography.py
+++ Homography/homography.py
@@ -7,9 +7,7 @@ import time
 img_read = cv2.imread("./Socket No BG.png")
 
 cap = cv2.VideoCapture("/dev/video2")
-while cap is None:
-    print("cap")
-    cap = cv2.VideoCapture("/dev/video2")
+assert cap.isOpened()
 
 # cap = cv2.VideoCapture(0)
 
@@ -19,9 +17,9 @@ kp_image, desc_image = sift.detectAndCompute(img_read, None)
 
 #make cv2 windows
 cv2.namedWindow("Homography")
-cv2.moveWindow("Homography", 10, 10)
-cv2.namedWindow("imgC")
-cv2.moveWindow("imgC", 500, 100)
+cv2.moveWindow("Homography", 10, 20)
+cv2.namedWindow("matchesframe")
+cv2.moveWindow("matchesframe", 10, 400)
 
 # Feature matching
 index_params = dict(algorithm=0, trees=5)
@@ -32,8 +30,8 @@ img_gray = cv2.cvtColor(img_read, cv2.COLOR_BGR2GRAY)
 img = img_read
 cv2.drawKeypoints(img_gray, kp_image, img)
 
-# time.sleep(1)
-counter = 0
+centroid = []
+
 while True:
     _, frame = cap.read()
     if (frame is not None):
@@ -42,9 +40,6 @@ while True:
         imgC = frame.copy()
 
         kp_grayframe, desc_grayframe = sift.detectAndCompute(grayframe, None)
-
-        # if(des1 is not None and len(des1)>2 and des2 is not None and len(des2)>2):
-        # matches = flann.knnMatch(des1,des2,k=2)
 
         if (desc_image is not None and len(desc_image) > 2
                 and desc_grayframe is not None and len(desc_grayframe) > 2):
@@ -57,12 +52,12 @@ while True:
             if m.distance < 0.5 * n.distance:
                 good_points.append(m)
 
-        img3 = cv2.drawMatches(img_gray, kp_image, grayframe, kp_grayframe,
-                               good_points, grayframe, 1)
-        cv2.imshow("img3", img3)
+        matchesframe = cv2.drawMatches(img_gray, kp_image, grayframe,
+                                       kp_grayframe, good_points, grayframe, 1)
+        cv2.imshow("matchesframe", matchesframe)
 
         #homography
-        if len(good_points) > 9:
+        if len(good_points) > 10:
             query_pts = np.float32([
                 kp_image[m.queryIdx].pt for m in good_points
             ]).reshape(-1, 1, 2)
@@ -97,7 +92,7 @@ while True:
                 # cv2.circle(frame, (int(pDot[0, 0]) + train_pts[0],
                 #                    int(pDot[0, 1]) + train_pts[1]), 5,
                 #            (0, 0, 255), 2)
-
+                # print(train_pts)
                 #############################################
                 # cv2.imshow("img", img)
 
@@ -112,17 +107,28 @@ while True:
                 homography = cv2.polylines(frame, [np.int32(dst)], True,
                                            (255, 0, 0), 3)
 
+                centroid.append(np.int32(dst))
+
+                # print(dst)
+
                 cv2.imshow("Homography", homography)
         # else:
-        # cv2.imshow("grayFrame", grayframe)
+        # cv2.imshow("grayframe", grayframe)
 
         cv2.drawKeypoints(grayframe, kp_grayframe, frame)
         # cv2.imshow("img", grayframe)
-        # cv2.imshow("imgC", imgC)
+        cv2.imshow("imgC", imgC)
         # cv2.imshow("Frame", frame)
 
         key = cv2.waitKey(1)
         if key == 27:
+            # print(centroid)
+            # np_centroid = np.array(centroid)
+            # mean = np.mean(np_centroid, axis=0)
+            # sd = np.std(np_centroid, axis=0)
+            # final_list = [x for x in centroid if (x > mean - 2 * sd)]
+            # final_list = [x for x in final_list if (x < mean + 2 * sd)]
+            # print(final_list)
             break
 
 cap.release()
